d6d59ef92acb79b1064f561c7475f633a3f09edd
Bandar Deterjen <bandardeterjen@gmail.com>
2025-03-14 14:15:17 +0700

    Update socpro-kreolin.js

--- kreolin/socpro-kreolin.js
+++ kreolin/socpro-kreolin.js
@@ -1,4 +1,13 @@
-// script.js
+// 1 liter botol https://images.tokopedia.net/img/cache/100-square/VqbcmM/2022/3/23/663ea769-d2d5-4d39-8701-463e5f77fa8c.jpg.webp?ect=4g
+// 1 liter jerigen https://images.tokopedia.net/img/cache/100-square/VqbcmM/2022/3/23/a6d3ce8e-533f-41f1-ade4-279595bdc469.jpg.webp?ect=4g
+// 500 ml botol https://images.tokopedia.net/img/cache/100-square/VqbcmM/2022/3/23/000ef776-141f-424a-a529-e203159836f0.jpg.webp?ect=4g
+// 5 liter https://images.tokopedia.net/img/cache/100-square/VqbcmM/2022/3/23/bcb74036-e640-4645-af10-14c15e50e17e.jpg.webp?ect=4g
+// Link Tokped
+// 1 liter dan 500 ml https://www.tokopedia.com/lapaklaundry/kreolin-pembersih-lantai-toilet
+// 5 liter https://www.tokopedia.com/lapaklaundry/kreolin-5-liter-pembersih-lantai
+// Link Shopee
+// 1 liter dan 500 ml
+// 5 liter
 
 // Array of social proof data
 const socialProofData = [
